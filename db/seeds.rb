dccbc4e00b21719ff770f4e4a923c51caec07cfd
Sachin Metkari <sachinmetkari89@gmail.com>
2022-04-04 23:11:29 +0530

    Add parking lots test data in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+50.times do |number|
+  parking_lot = ParkingLot.new
+  parking_lot.slot_number = 'A' + (number + 1).to_s
+  parking_lot.status = 'unallocated'
+  parking_lot.distance_from_entry_point = (10 * (number + 1))
+  parking_lot.save!
+end
+
+50.times do |number|
+  parking_lot = ParkingLot.new
+  parking_lot.slot_number = 'B' + (number + 1).to_s
+  parking_lot.status = 'unallocated'
+  parking_lot.distance_from_entry_point = (10 * (number + 1))
+  parking_lot.save!
+end
